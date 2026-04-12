e52f93f350be97008e4bb877d3465a3c14223d4b
VIKETA <crystintell@gmail.com>
2026-04-12 13:14:44 +0100

    commit on sunday 12/04/2026

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -81,7 +81,6 @@ router.post('/auth', cors({ origin: '*' }), async (req, res) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specified methods
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow specified headers
     let data = req.body
-    console.log(data)
     await pool.connect().then(async (r) => {
         if (r._connected) {
             query="UPDATE warehoustidentity SET auth=$1 WHERE  identityid=$2 "
@@ -106,4 +105,139 @@ router.post('/auth', cors({ origin: '*' }), async (req, res) => {
         }
     })
 })
+
+
+
+
+router.post('/delIdentity', cors({ origin: '*' }), async (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins, or specify a specific origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specified methods
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow specified headers
+    let data = req.body
+   console.log(data)   
+    await pool.connect().then(async (r) => {
+        if (r._connected) {
+            query="DELETE FROM  warehoustidentity  WHERE  identityid=$1 "
+            r.query(query,[data.identityid],(error,results)=>{
+                if(error){
+                               console.log("The error ", error)
+                    r.release();
+                    return res.status(400).json({ message: error.detail })
+                }else{
+                    if(results.rowCount>0){
+                             
+                    r.release();
+                    return res.status(200).json({ success: 'Object successfully removed' })
+                    }else{
+                            r.release();
+                    return res.status(400).json({ message: "Internal error has prevented the system from fulfilling your request" })
+                    }
+                }
+            })
+        }else{
+              return res.status(400).json({ message: "Unable to connection to the Database" })
+        }
+    })
+})
+
+
+
+
+
+router.post('/updateIdentity', cors({ origin: '*' }), async (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins, or specify a specific origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specified methods
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow specified headers
+    let data = req.body
+    console.log(data)
+    await pool.connect().then(async (r) => {
+        if (r._connected) {
+            query="UPDATE warehoustidentity SET name=$1,decription=$2,date=$3 WHERE  identityid=$4 "
+            r.query(query,[data.name,data.describe,data.date,data.identity],(error,results)=>{
+                if(error){
+                               console.log("The error ", error)
+                    r.release();
+                    return res.status(400).json({ message: error.detail })
+                }else{
+                    if(results.rowCount>0){    
+                    r.release();
+                    return res.status(200).json({ success: 'Authorisation successful' })
+                    }else{
+                            r.release();
+                    return res.status(400).json({ message: "Internal error has prevented the system from fulfilling your request" })
+                    }
+                }
+            })
+        }else{
+              return res.status(400).json({ message: "Unable to connection to the Database" })
+        }
+    })
+})
+
+
+router.get('/listwarehouses', cors({ origin: '*' }), async (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins, or specify a specific origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specified methods
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow specified headers
+
+    await pool.connect().then(async (r) => {
+        if (r._connected) {
+            query=" SELECT warehouse.serialnumber,warehouse.identityid,warehouse.warehousename,warehouse.location,warehouse.digitaladdress,warehouse.decription,warehouse.date,warehouse.isopened,warehoustidentity.name FROM warehouse LEFT JOIN warehoustidentity ON warehouse.identityid=warehoustidentity.identityid"
+            r.query(query,(error,results)=>{
+                if(error){
+                               console.log("The error ", error)
+                    r.release();
+                    return res.status(400).json({ message: error.detail })
+                }else{
+                    if(results.rows.length>0){    
+                    r.release();
+                    return res.status(200).json({ data: results.rows })
+                    }else{
+                            r.release();
+                    return res.status(400).json({ message: "No warehouse details. Create a new Warehouse" })
+                    }
+                }
+            })
+        }else{
+              return res.status(400).json({ message: "Unable to connection to the Database" })
+        }
+    })
+})
+
+
+
+
+// saveWearhouse
+
+router.post('/saveWearhouse', cors({ origin: '*' }), async (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins, or specify a specific origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specified methods
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow specified headers
+    let data = req.body
+    console.log(data)
+    await pool.connect().then(async (r) => {
+        if (r._connected) {
+            console.log(data)
+            query = "INSERT INTO warehouse(serialnumber,identityid,warehousename,location,digitaladdress,decription,date,isopened)VALUES($1,$2,$3,$4,$5,$6,$7,$8)"
+            r.query(query, [data.warehouseSerial, data.identity, data.warehouseTitle, data.warehouseLocation,data.warehousedigialAddress,data.warehouseDescription,data.date,false], (error, results) => {
+                if (error) {
+                    console.log("The error ", error)
+                    r.release();
+                    return res.status(400).json({ message: error.detail })
+                } else {
+                    if (results.rowCount > 0) {
+                        r.release();
+                        return res.status(200).json({ success: "Identity successfully created" })
+                    } else {
+                        r.release()
+                        return res.status(200).json({ message: "Internal error has occured. Try again" })
+                    }
+                }
+            })
+
+        }
+    })
+})
+
+
 module.exports = router
